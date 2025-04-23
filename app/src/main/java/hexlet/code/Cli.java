de2320a449d9429a68fc5fbb484a20209e460d10
olikkks <h_o@bk.ru>
2025-04-23 20:44:38 +0300

    add Cli, add tasks to build.gradle.kts

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void Client() {
+        System.out.println("May I have your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + " !");
+    }
+}
