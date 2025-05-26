08dadaa795dd7ed0993fd3a1378c62f7b01055c7
olikkks <h_o@bk.ru>
2025-05-26 22:40:59 +0300

    add GrstComDivisor update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,14 +2,13 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Evenness;
-
-import hexlet.code.Engine;
+import hexlet.code.games.GrstComDivisor;
 
 import java.util.Scanner;
 
 class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter. \n1 - Greet \n2 - Even \n3 - Calc \n0 - Exit");
+        System.out.println("Please enter the game number and press Enter. \n1 - Greet \n2 - Even \n3 - Calc \n4 - GCD \n0 - Exit");
         Scanner gameNumber = new Scanner(System.in);
         String gameNum = gameNumber.nextLine();
         System.out.println("Your choice: " + gameNum);
@@ -26,6 +25,12 @@ class App {
             case "3":
                 game = new Calc();
                 Engine.engine(game);
+                break;
+            case "4":
+                game = new GrstComDivisor();
+                Engine.engine(game);
+                break;
+            case "0":
                 break;
             default:
                 System.out.println("unexpected number");
---
+++ app/src/main/java/hexlet/code/games/GrstComDivisor.java
@@ -0,0 +1,59 @@
+package hexlet.code.games;
+import hexlet.code.GameTaskAndCorrectAnswer;
+import java.util.Random;
+
+public class GrstComDivisor implements GameTaskAndCorrectAnswer {
+
+    private final String task = "Find the greatest common divisor of given numbers.";
+    private String taskQuestion;
+    private String correctAnswer;
+
+
+    public GrstComDivisor() {
+        FindGCD();
+    }
+
+    private void FindGCD() {
+        Random random1 = new Random();
+        int dividend1 = random1.nextInt(100);
+        Random random2 = new Random();
+        int dividend2 = random2.nextInt(100);
+
+        int a = dividend1;
+        int b = dividend2;
+        int gcd;
+        int reminder;
+        if (b == 0) {
+            gcd = a;
+        } else while (b != 0) {
+            reminder = a % b;
+            a = b;
+            b = reminder;
+        }
+        gcd = a;
+
+        taskQuestion = "Question: " + dividend1 + " " + dividend2;
+        correctAnswer = "" + gcd;
+    }
+
+    @Override
+    public String getTask() {
+        return task;
+    }
+
+    @Override
+    public String getSelectedTaskQuestion() {
+            return taskQuestion;
+    }
+
+    @Override
+    public String getCorrectAnswer() {
+            return correctAnswer;
+    }
+
+    @Override
+    public void update() {
+            FindGCD();
+    }
+
+}
