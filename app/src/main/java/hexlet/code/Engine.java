091b7d980c2c7162805b2c8a2dc0da3b4b3aa3dd
olikkks <h_o@bk.ru>
2025-06-10 17:34:08 +0300

    update LoggerConfig {} for if, detalize import, update Engine remove empty lines

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -13,12 +13,10 @@ public class Engine {
     public static void engine(String gameNum, String name) {
 
         Logger logger = Logger.getLogger(Engine.class.getName());
-
         String selectedTask;
         String clientsSolution;
         String selectedTaskQuestion;
         String selectedCorrectAnswer;
-
         final int numOfRepeats = 3;
         final int endOfRepeats = 4;
         int i = 1;
@@ -31,17 +29,13 @@ public class Engine {
                 Evenness even = new Evenness();
                 selectedTask = even.getTask();
                 logger.info(selectedTask);
-
                 for (i = 1; i <= numOfRepeats; i++) {
                     selectedTaskQuestion = even.getTaskQuestion();
                     logger.info(selectedTaskQuestion);
-
                     Scanner clientAnswer = new Scanner(System.in);
                     clientsSolution = clientAnswer.nextLine();
                     logger.info("Your answer: " + clientsSolution);
-
                     selectedCorrectAnswer = even.getCorrectAnswer();
-
                     if (clientsSolution.equals(selectedCorrectAnswer)) {
                         logger.info("Correct!");
                         even.update();
@@ -58,17 +52,13 @@ public class Engine {
                 Calc calc = new Calc();
                 selectedTask = calc.getTask();
                 logger.info(selectedTask);
-
                 for (i = 1; i <= numOfRepeats; i++) {
                     selectedTaskQuestion = calc.getTaskQuestion();
                     logger.info(selectedTaskQuestion);
-
                     Scanner clientAnswer = new Scanner(System.in);
                     clientsSolution = clientAnswer.nextLine();
                     logger.info("Your answer: " + clientsSolution);
-
                     selectedCorrectAnswer = calc.getCorrectAnswer();
-
                     if (clientsSolution.equals(selectedCorrectAnswer)) {
                         logger.info("Correct!");
                         calc.update();
@@ -85,17 +75,13 @@ public class Engine {
                 GrstComDivisor gcdiv = new GrstComDivisor();
                 selectedTask = gcdiv.getTask();
                 logger.info(selectedTask);
-
                 for (i = 1; i <= numOfRepeats; i++) {
                     selectedTaskQuestion = gcdiv.getTaskQuestion();
                     logger.info(selectedTaskQuestion);
-
                     Scanner clientAnswer = new Scanner(System.in);
                     clientsSolution = clientAnswer.nextLine();
                     logger.info("Your answer: " + clientsSolution);
-
                     selectedCorrectAnswer = gcdiv.getCorrectAnswer();
-
                     if (clientsSolution.equals(selectedCorrectAnswer)) {
                         logger.info("Correct!");
                         gcdiv.update();
@@ -112,17 +98,13 @@ public class Engine {
                 Progression pr = new Progression();
                 selectedTask = pr.getTask();
                 logger.info(selectedTask);
-
                 for (i = 1; i <= numOfRepeats; i++) {
                     selectedTaskQuestion = pr.getTaskQuestion();
                     logger.info(selectedTaskQuestion);
-
                     Scanner clientAnswer = new Scanner(System.in);
                     clientsSolution = clientAnswer.nextLine();
                     logger.info("Your answer: " + clientsSolution);
-
                     selectedCorrectAnswer = pr.getCorrectAnswer();
-
                     if (clientsSolution.equals(selectedCorrectAnswer)) {
                         logger.info("Correct!");
                         pr.update();
@@ -139,17 +121,13 @@ public class Engine {
                 Prime prime = new Prime();
                 selectedTask = prime.getTask();
                 logger.info(selectedTask);
-
                 for (i = 1; i <= numOfRepeats; i++) {
                     selectedTaskQuestion = prime.getTaskQuestion();
                     logger.info(selectedTaskQuestion);
-
                     Scanner clientAnswer = new Scanner(System.in);
                     clientsSolution = clientAnswer.nextLine();
                     logger.info("Your answer: " + clientsSolution);
-
                     selectedCorrectAnswer = prime.getCorrectAnswer();
-
                     if (clientsSolution.equals(selectedCorrectAnswer)) {
                         logger.info("Correct!");
                         prime.update();
@@ -166,10 +144,8 @@ public class Engine {
                 logger.info("Exit");
                 break;
             default:
-                //System.out.println("unexpected number");
                 logger.info("unexpected number");
         }
-
         if (i == endOfRepeats) {
             logger.info("Congratulations, " + name + "!");
         }
--- app/src/main/java/hexlet/code/LoggerConfig.java
+++ app/src/main/java/hexlet/code/LoggerConfig.java
@@ -1,12 +1,18 @@
 package hexlet.code;
 
-import java.util.logging.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
 
 public class LoggerConfig {
     private static boolean initialized = false;
 
     public static void init() {
-        if (initialized) return;
+        if (initialized) {
+            return;
+        }
         Logger rootLogger = Logger.getLogger("");
         for (Handler handler : rootLogger.getHandlers()) {
             rootLogger.removeHandler(handler);
