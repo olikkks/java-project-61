b2364fb6887fa16392b390d509d58ed737087514
olikkks <h_o@bk.ru>
2025-06-27 14:06:24 +0300

    update Makefile and build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,20 +8,20 @@ plugins {
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-application { mainClass = "hexlet.code.App"}
+application { mainClass = "hexlet.code.App" }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
+//dependencies {
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//}
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
 
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
