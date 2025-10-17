ac3ec95ebcfd9bc6b5a915bb8210a791e4086fa9
olikkks <h_o@bk.ru>
2025-10-17 20:39:51 +0300

    update: Greet test

--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -8,6 +8,7 @@ public class Greet {
     public static void client() {
         Logger logger = Logger.getLogger(Greet.class.getName());
         logger.info("Welcome to the Brain Games! \nMay I have your name?");
+
         Scanner s = new Scanner(System.in);
         String name = s.nextLine();
         logger.info("Hello, " + name + "!");
